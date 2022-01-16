66f00722c6bcccafae6444275d32f1630404c8bf
jinpyoung <jinpyoung@treenod.com>
2022-01-16 23:24:24 +0900

    텍스트 스플리트 수정

--- TextSplitter/TxtSplitter.py
+++ TextSplitter/TxtSplitter.py
@@ -10,10 +10,17 @@ def folderMake(path):
         os.mkdir(path)
 
 # 데이터의 마지막 라인의 텍스트 추출하기
-def endTextLine(end_index, sub_data):
+def endTextLine(path, start_index, end_index, sub_data, number):
     reverseData = sub_data[::-1]
-    firstIndex = end_index - reverseData.find('\n', 0)
+    firstIndex = end_index - start_index - reverseData.find('\n', 0)
     end_text = sub_data[firstIndex:end_index]
+
+    if len(end_text) < 10:
+        logTxt = f'{str(number)}.txt 파일 이상 발견 : ' + str(len(end_text)) + '\n' + end_text + '\n----------------------------------------------------------\n'
+        fileMake(path, 'a', logTxt)
+    else:
+        logTxt = ''
+
     return end_text
 
 # 파일 생성 또는 수정
@@ -31,8 +38,9 @@ except FileExistsError:
 
 # final 디렉토리 생성하고 로그파일 생성  -------------------------------------------
 dir = "TextSplitter/final"
+logfile = dir + "/00_log.txt"
 folderMake(dir)
-fileMake(dir + "/00_log.txt", 'w', '로그파일이 생성되었습니다.')
+fileMake(logfile, 'w', '로그파일이 생성되었습니다.\n')
 
 maxChar = 5000  # 1화당 최대 글자수
 data_length = len(data)
@@ -41,22 +49,21 @@ endIdx = data.find('\n', maxChar) # maxChar 인덱스 이후에 나오는 첫번
 subData = data[startIdx:endIdx]
 
 # 텍스트 파일에 데이터 기록하고 저장하기
-num = 1
+num = 0
 while data_length > 0:
-    
-    endLineTxt = endTextLine(endIdx, subData)
-    print(str(num) + '-----------------------\n' + endLineTxt)
+
+    num += 1
+    endLineTxt = endTextLine(logfile, startIdx, endIdx, subData, num)
 
     if num < 10:
         filePath = dir + f'/0{str(num)}.txt'
     else:
         filePath = dir + f'/{str(num)}.txt'
 
-    print('저장 파일 경로 : ' + filePath)
+    # print('저장 파일 경로 : ' + filePath)
     fileMake(filePath, 'w', subData)
 
     # 파일 생성후 다음 파일 컨텐츠를 위한 인덱스 조정
-    num += 1
     data_length -= (endIdx - startIdx)
     startIdx = endIdx
     findIdx = startIdx + maxChar
@@ -66,5 +73,4 @@ while data_length > 0:
     else:
         endIdx = len(data)
 
-    subData = data[startIdx:endIdx]
-    endLineTxt = endTextLine(endIdx, subData)
+    subData = data[startIdx+1:endIdx]
