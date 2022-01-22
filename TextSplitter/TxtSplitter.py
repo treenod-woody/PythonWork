3c9346aee614d7e71f6cd075ec03df56f3a988d0
jinpyoung <jinpyoung@treenod.com>
2022-01-22 16:15:51 +0900

    스플리터 최신본

--- TextSplitter/TxtSplitter.py
+++ TextSplitter/TxtSplitter.py
@@ -1,3 +1,4 @@
+from cgitb import text
 import os
 import shutil
 from tkinter import *
@@ -32,7 +33,7 @@ def fileMake(path, state, content):
         file.write(content)
 
 # 텍스트 분리 작업 실행
-def main(origin_path, maxChar):
+def main(origin_path, save_dir, maxChar):
 
     # 00.txt 원본 파일의 내용 읽어오기
     try:
@@ -43,7 +44,7 @@ def main(origin_path, maxChar):
         print("00.txt 파일이 존재하지 않습니다.")
 
     # final 디렉토리 생성하고 로그파일 생성  -------------------------------------------
-    dir = "TextSplitter/final"
+    dir = save_dir + "/final"
     logfile = dir + "/00_log.txt"
     folderMake(dir)
     fileMake(logfile, 'w', '로그파일이 생성되었습니다.\n')
@@ -91,18 +92,41 @@ win.title("Text splitter")
 frame_file = Frame(win, relief="solid", bd=1)
 frame_file.pack(fill="x", padx=5, pady=5)
 
+label_filepath = Label(frame_file, text="원본 TXT 파일 : ")
+label_filepath.pack(side="left")
+
 e_filepath = Entry(frame_file, width=30)
 e_filepath.pack(side="left")
+e_filepath.insert(END, "C:/Users/USER/Desktop/00.txt")
+
+# 파일 저장 프레임 -----------------------------------
+frame_dir = Frame(win, relief="solid", bd=1)
+frame_dir.pack(fill="x", padx=5, pady=5)
+
+label_savepath = Label(frame_dir, text="저장 경로 선택 : ")
+label_savepath.pack(side="left")
+
+e_savepath = Entry(frame_dir, width=30)
+e_savepath.pack(side="left")
+e_savepath.insert(END, "C:/Users/USER/Desktop")
 
 def get_file():
     file = filedialog.askopenfilename(title="원본 txt파일을 선택하세요.", \
         filetypes=(("TXT 파일", "*.txt"), ("모든 파일", "*.*")), \
-            initialdir="TextSplitter")   # 최초 시작 경로는 C:/
+            initialdir="C:/")   # 최초 시작 경로는 C:/
 
     e_filepath.insert(END, file)
 
-btn_getfile = Button(frame_file, padx=2, pady=2, text="파일선택", command=get_file)
+def save_dir():
+    dir = filedialog.askdirectory(title="생성된 파일들이 저장될 폴더를 선택해주세요.", initialdir="C:/")
+    e_savepath.delete(0, "end")
+    e_savepath.insert(END, dir)
+
+btn_getfile = Button(frame_file, padx=2, pady=2, text="원본파일 선택", command=get_file)
 btn_getfile.pack(side="right")
+
+btn_savefile = Button(frame_dir, padx=2, pady=2, text="저장경로 선택", command=save_dir)
+btn_savefile.pack(side="right")
 
 # 최대 텍스트값 입력 프레임 ---------------------------
 frame_info = Frame(win, relief="solid", bd=1)
@@ -118,14 +142,17 @@ e.insert(END, "5000")
 # 버튼 실행 -----------------------------------------
 def btncmd():
     origin_filepath = e_filepath.get()  # 원본 파일 경로 할당
+    final_savepath = e_savepath.get()
     maxvalue = int(e.get())  # 최대 텍스트값 정보 할당
     if origin_filepath == "":
         msgbox.showinfo("알림","선택된 파일이 없습니다. 파일을 선택해주세요.")
+    if final_savepath == "":
+        msgbox.showinfo("알림","저장 경로를 선택해주세요.")
     elif maxvalue < 4500 or maxvalue > 200000:
         msgbox.showinfo("알림", "값은 4500 ~ 200000 사이의 값이어야 합니다.")
     else:
         win.quit()
-        # main(origin_filepath, maxvalue)
+        main(origin_filepath, final_savepath, maxvalue)
 
 btn_confirm = Button(win, padx=10, pady=4, text='실행하기', command=btncmd)
 btn_confirm.pack(padx=5, pady=5)
