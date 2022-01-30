2a88a78b9eff1f5ae0bc3bbc5a515cf8a68098c7
jinpyoung <jinpyoung@treenod.com>
2022-01-30 23:44:06 +0900

    수정버전

--- FileOrderChecker.py
+++ FileOrderChecker.py
@@ -1,6 +1,8 @@
+from ast import For
 from tkinter import *
 from tkinter import filedialog
 import tkinter.messagebox as msgbox
+import re
 import os
 
 # ==========================================================
@@ -19,7 +21,7 @@ labelRef.pack(side="left")
 
 e_openDir = Entry(frame_dir, width=30)
 e_openDir.pack(side="left", padx=4, pady=4)
-e_openDir.insert(END, "폴더를 선택해주세요.")
+e_openDir.insert(END, "검사 폴더를 선택하세요")
 
 def open_dir():
     dir = filedialog.askdirectory(title="파일을 검사할 폴더를 선택하세요.")
@@ -29,25 +31,13 @@ def open_dir():
 btn_openDir = Button(frame_dir, padx=4, pady=4, text="폴더선택", command=open_dir)
 btn_openDir.pack(side="right")
 
-# 검사 회차 선택 프레임 ----------------------------
-frame_num = Frame(win, relief="solid", bd=1)
-frame_num.pack(fill="x", padx=10, pady=10)
-
-labelRef_num = Label(frame_num, text="시작회차(숫자만 입력) : ")
-labelRef_num.pack(side="left")
-
-e_startNum = Entry(frame_num, width=10)
-e_startNum.pack(side="left", padx=4, pady=4)
-e_startNum.insert(END, "1")
-
 # 버튼 실행 --------------------------------------
 def btncmd():
 
     dir = e_openDir.get()   # 검사할 폴더 경로
-    sep_txt = e_startNum.get()
-    cnt = int(sep_txt)
     logfile = dir + "/00_log.txt"   # 로그 파일
-    fileNames = []
+    fileList = []   # 폴더내 모든 파일 리스트
+    numbers = []    # 모든 회차 정보
 
     try:
         if os.path.isfile(logfile): # 로그 파일이 있다면 제거한후 검사 시작
@@ -55,36 +45,33 @@ def btncmd():
 
         fileList = os.listdir(dir)
 
+        # log 파일 생성
+        logfile = dir + "/00_log.txt"
+        fileMake(logfile, 'w', '검사를 시작합니다.\n\n')
+
         for f in fileList:
             if f==".DS_Store":
                 continue
             else:
-                fileNames.append(f)
+                # N화를 모두 추출한 후 숫자만 남김 -----------
+                regex = re.compile(r'\d+\w+')
+                mo = regex.search(f).group()
+                num = re.findall(r'\d+', mo)
+                for n in num:
+                    numbers.append(int(n))
 
-        fileNames.sort()
-        fname = str(fileNames[0])    # 1화 파일명
+        numbers.sort()
+        
+        for i in range(0, len(fileList)-1):
+            value = numbers[0] + i
+            if numbers[i] == value:
+                continue
+            else:
+                content = str(value) + "화에서 문제가 발견되었습니다.\n-------------------------------\n"
+                fileMake(logfile, 'a', content)
 
-        try:
-            values = fname.split(sep=sep_txt)   # 1을 기준으로 앞이름과 뒷이름으로 분리
+        win.quit()
 
-            # log 파일 생성
-            logfile = dir + "/00_log.txt"
-            fileMake(logfile, 'w', '검사를 시작합니다.\n\n')
-
-            # 검증 : fileNames <-> values[0] + str(cnt) + values[1]
-            for v in fileNames:
-                check_fname = values[0] + str(cnt) + values[1]
-                if(v == check_fname):
-                    cnt += 1
-                else:
-                    content = v + " : 파일명 이상 발견\n================================\n" 
-                    fileMake(logfile, 'a', content)
-                    cnt += 1
-
-            win.quit()
-
-        except:
-            msgbox.showinfo("알림", "시작 회차 정보가\n잘못 입력되었습니다.")
     except:
         msgbox.showinfo("알림", "폴더 경로를 찾을 수 없습니다.\n검사할 폴더를 다시 선택해주세요.")
 
