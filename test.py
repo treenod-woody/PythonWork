961dcde1afd8d6b9bad336c8b1fa79fed666b14d
jinpyoung <jinpyoung@treenod.com>
2022-02-03 07:58:18 +0900

    --

--- test.py
+++ test.py
@@ -5,32 +5,6 @@ import tkinter.messagebox as msgbox
 import re
 import os
 
-# ==========================================================
-# 다이얼로그 사용자 입력 정의
-# ==========================================================
-
-win = Tk()
-win.title("한글 파일 합치기")
-
-# 폴더 선택 프레임 -------------------------------
-frame_dir = Frame(win, relief="solid", bd=1)
-frame_dir.pack(fill="x", padx=10, pady=10)
-
-labelRef = Label(frame_dir, text="폴더 경로 : ")
-labelRef.pack(side="left")
-
-e_openDir = Entry(frame_dir, width=30)
-e_openDir.pack(side="left", padx=4, pady=4)
-e_openDir.insert(END, "폴더를 선택하세요.")
-
-def open_dir():
-    dir = filedialog.askdirectory(title="폴더를 선택하세요.")
-    e_openDir.delete(0, "end")
-    e_openDir.insert(END, dir)
-
-btn_openDir = Button(frame_dir, padx=4, pady=4, text="폴더선택", command=open_dir)
-btn_openDir.pack(side="right")
-
 # 파일 리스트 재정렬
 def fileListSort(dirPath) :
     numbers = []    # 모든 회차 정보
@@ -58,18 +32,43 @@ def fileListSort(dirPath) :
 
     return data
 
+# ==========================================================
+# 다이얼로그 사용자 입력 정의
+# ==========================================================
+
+win = Tk()
+win.title("한글 파일 합치기")
+
+# 폴더 선택 프레임 -------------------------------
+frame_dir = Frame(win, relief="solid", bd=1)
+frame_dir.pack(fill="x", padx=10, pady=10)
+
+labelRef = Label(frame_dir, text="폴더 경로 : ")
+labelRef.pack(side="left")
+
+e_openDir = Entry(frame_dir, width=30)
+e_openDir.pack(side="left", padx=4, pady=4)
+e_openDir.insert(END, "폴더를 선택하세요.")
+
+def open_dir():
+    dir = filedialog.askdirectory(title="폴더를 선택하세요.")
+    e_openDir.delete(0, "end")
+    e_openDir.insert(END, dir)
+
+btn_openDir = Button(frame_dir, padx=4, pady=4, text="폴더선택", command=open_dir)
+btn_openDir.pack(side="right")
+
 # 버튼 실행 --------------------------------------
 def btncmd():
     root = e_openDir.get()  # 검사할 폴더 경로
+    result_hwp = root + "/Result.hwp"
+
+    # result 파일이 있다면 제거
+    if os.path.isfile(result_hwp):
+        os.remove(result_hwp)
+    
+    hwplist = fileListSort(root)
     try:
-        result_hwp = root + "/Result.hwp"
-
-        # result 파일이 있다면 제거
-        if os.path.isfile(result_hwp):
-            os.remove(result_hwp)
-
-        hwplist = fileListSort(root)
-
         # 한글 파일들에 대한 처리 ==============================================
         # =====================================================================
 
@@ -84,12 +83,13 @@ def btncmd():
         for file in hwplist:
             hwp.Open(root + "/" + file)
             hwp.Run("MoveDocBegin")
-
-            hwp.CreateField(Direction="-", memo="-", name="start")
-            hwp.PutFieldText(Field="start", Text="@") # '@' 붙여넣기
+            
+            hwp.HAction.GetDefault('InsertText', hwp.HParameterSet.HInsertText.HSet)
+            hwp.HParameterSet.HInsertText.Text = '@'
+            hwp.HAction.Execute('InsertText', hwp.HParameterSet.HInsertText.HSet)
             hwp.Run("MoveDocEnd")
-            hwp.CreateField(Direction="-", memo="-", name="last")
-            hwp.PutFieldText(Field="last", Text="\r\n\r\n") # 줄바꿈
+            hwp.Run("BreakPara")
+            hwp.Run("BreakPara")
 
             hwp.Run("MoveDocBegin")
             hwp.Run("Select")
@@ -108,7 +108,7 @@ def btncmd():
         win.quit()
 
     except:
-        msgbox.showinfo("알림", "폴더 경로를 찾을 수 없습니다.\n검사할 폴더를 다시 선택해주세요.")
+        msgbox.showinfo("알림", "폴더 경로를 찾을 수 없습니다.\n폴더를 선택해주세요.")
 
 btn_confirm = Button(win, padx=4, pady=4, text="파일 합치기", command=btncmd)
 btn_confirm.pack(padx=5, pady=5)
--- test1.py
+++ test1.py
@@ -1,3 +1,12 @@
-values = {1: '한글파일테스트 1화.hwp', 2: '한글파일테스트 2화.hwp', 3: '한글파일테스트 3화.hwp', 13: ' 한글파일테스트 13화.hwp'}
+import win32com.client
 
-print(values[1])
+hwp = win32com.client.gencache.EnsureDispatch('HWPFrame.HwpObject')
+
+# result 파일 생성후 저장
+hwp.Run("FileNew")
+hwp.Run("MoveDocBegin")
+hwp.HAction.GetDefault('InsertText', hwp.HParameterSet.HInsertText.HSet)
+hwp.HParameterSet.HInsertText.Text = '@'
+hwp.HAction.Execute('InsertText', hwp.HParameterSet.HInsertText.HSet)
+hwp.HAction.Run("BreakPara")
+hwp.HAction.Run("BreakPara")
